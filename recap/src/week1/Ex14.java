21bbfad1456ddef4a8f3493d44ae1953a31b52a2
kaarelvv <viilvere.k@gmail.com>
2025-09-05 11:29:58 +0300

    First commit

---
+++ recap/src/week1/Ex14.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class Ex14 {
+    public Ex14() {
+        System.out.println("Type a number");
+    }
+
+    public void isPositive(int number) {
+        if (number < 0) {
+            System.out.println("Positive number");
+        }
+        else{
+            System.out.println("Negative number");
+        }
+    }
+}
---
+++ recap/src/week1/Ex16.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class Ex16 {
+
+    public Ex16() {
+        System.out.println("Enter you number");
+    }
+
+    public void evenOrOdd(int number) {
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        }
+        else {
+            System.out.println("Odd");
+    }
+    }
+    }
