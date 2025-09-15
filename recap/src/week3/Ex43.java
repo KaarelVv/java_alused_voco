d239790f66ec52e10d6c624767b8a157ea26bfa1
Kaarel Viilvere <viilvere.k@gmail.com>
2025-09-15 12:35:13 +0300

    week3-Ex43, Ex45

---
+++ recap/src/week3/Ex43.java
@@ -0,0 +1,10 @@
+package week3;
+
+public class Ex43 {
+
+    public static int sum(int number1, int number2, int number3, int number4) {
+       int sum = number1 + number2 + number3 + number4;
+
+        return sum;
+    }
+}
--- recap/src/week3/Main.java
+++ recap/src/week3/Main.java
@@ -1,7 +1,14 @@
 package week3;
 
+import static week3.Ex43.sum;
+import static week3.Ex45.greatest;
+
 public class Main {
     public static void main(String[] args) {
+//        int answer = sum(4, 3, 6, 1);
+//        System.out.println("sum: " + answer);
 
+        int answer =  greatest(2, 7, 3);
+        System.out.println("Greatest: " + answer);
     }
 }
