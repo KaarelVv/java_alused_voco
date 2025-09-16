d8ea30d68098e8fc733fd6c816e1199f7fa8b99d
Kaarel Viilvere <viilvere.k@gmail.com>
2025-09-16 13:01:36 +0300

    week5-Ex86

---
+++ recap/src/week5/lyyra_card_and_cash_register/LyyraCard.java
@@ -0,0 +1,27 @@
+package week5.lyyra_card_and_cash_register;
+
+public class LyyraCard {
+    private double balance;
+
+    public LyyraCard(double balance) {
+        this.balance = balance;
+    }
+
+    public double balance() {
+        return this.balance;
+    }
+
+    public void loadMoney(double amount) {
+        this.balance += amount;
+    }
+
+    public boolean pay(double amount){
+        if(this.balance < amount){
+            return false;
+        }else {
+            this.balance -= amount;
+            return true;
+        }
+
+    }
+}
